4a0bb1231f46bb225f7f68c36d1dd35ab0f1669b
bateiko <bateiko0713@gmail.com>
2020-05-23 11:35:05 +0300

    funcrion close_server was added

--- ka72/Bateiko Eduard/project2/client.py
+++ ka72/Bateiko Eduard/project2/client.py
@@ -44,7 +44,7 @@ def on_closing(event=None):
 
 
 HOST = '127.0.0.1'
-PORT = 33000
+PORT = 33001
 
 BUFSIZE = 1024
 ADDR = (HOST, PORT)
--- ka72/Bateiko Eduard/project2/server.py
+++ ka72/Bateiko Eduard/project2/server.py
@@ -1,13 +1,14 @@
-from socket import AF_INET, socket, SOCK_STREAM
+from socket import AF_INET, socket, SOCK_STREAM, SHUT_RDWR
 from threading import Thread
 import block
 import json
 
 clients = {}
 addresses = {}
+threads = []
 
 HOST = ''
-PORT = 33000
+PORT = 33001
 BUFSIZE = 1024 ** 2
 ADDR = (HOST, PORT)
 SERVER = socket(AF_INET, SOCK_STREAM)
@@ -24,8 +25,9 @@ def accept_connections():
         client.send(bytes(f"Hello dear friend!!\n{line_with_question}", 'utf-8'))
 
         clients[client] = client_address
-        Thread(target=something_doing, args=(client,)).start()
-
+        new_thread = Thread(target=something_doing, args=(client,))
+        new_thread.start()
+        threads.append(new_thread)
 
 def add_block(client):
     recieve_massage = client.recv(BUFSIZE).decode('utf-8').replace("'", '"')
@@ -41,11 +43,11 @@ def something_doing(client):
         recieve_massage = client.recv(BUFSIZE).decode('utf-8')
 
         if recieve_massage == "0":
+            print(f"CLIENT WAS CLOSED:\n{clients[client]}")
             client.send(b"quit")
-            print(f"len:\t{len(clients)}")
             del clients[client]
             client.close()
-            print(f"len:\t{len(clients)}")
+
             break
 
         elif recieve_massage == '1':
@@ -64,10 +66,33 @@ def something_doing(client):
             client.send(bytes(message, 'utf-8'))
 
 
+def close_server():
+    while True:
+        message = input("Enter action:\t")
+        if message == 'close':
+            for client in list(clients):
+                print(f"CLIENT WAS CLOSED:\n{clients[client]}")
+                client.send(b"quit")
+                del clients[client]
+                client.close()
+
+            # for thread_ in threads:
+            #     thread_.close()
+            SERVER.close()
+            SERVER.shutdown(SHUT_RDWR)
+            return
+
+
 if __name__ == "__main__":
     SERVER.listen(5)
-    print("wainting connection...")
+    print("Waiting connection...")
+
     ACCEPT_THREAD = Thread(target=accept_connections)
+    # CLOSE_SERVER = Thread(target=close_server)
+
     ACCEPT_THREAD.start()
+    # CLOSE_SERVER.start()
+
     ACCEPT_THREAD.join()
+
     SERVER.close()
